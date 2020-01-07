e96ec1923b464bd53c6c29f23f1b0115f40f0b0f
15011217967 <zh280266967@126.com>
2020-01-07 12:27:52 +0800

    新增字符串方法

--- js/String.js
+++ js/String.js
@@ -1,2 +1,25 @@
 let string = [
+  // 1.考察charAt方法
+  "let str = 'abcdefg';，如何取到str中c这个值？（尽量使用字符串方法）",
+  // 2.考察charCodeAt方法
+  "let str = '0123456789';，str中7对应的Unicode 编码是多少？（尽量使用字符串方法）",
+  // 3.考察indexOf和lastIndexOf方法
+  "let str = 'acb123cba321bca231ccc331v';，分别找到第一个3的位置和最后一个c的位置？（尽量使用字符串方法）",
+  // 4.考察match方法
+  "let str = 'acb123cba321bca231ccc331v';，将str中数字字符串的项（如：123，321），装进arr数组中？（尽量使用字符串方法）",
+  // 5.考察replace方法
+  "let str = '订单号：20190107123265 操作人：张三';，将订单号这串数字，替换成 <span>20190107123265</span>（尽量使用字符串方法）",
+
+  // 6.考察slice方法
+  "let str = 'abcdefg';，使用slice方法，截取str中的def字符？（尽量使用字符串方法）",
+  // 7.考察substr方法
+  "let str = 'abcdefg';，使用substr方法，截取str中的def字符？（尽量使用字符串方法）",
+  // 8.考察substring方法
+  "let str = 'abcdefg';，使用substring方法，截取str中的def字符？（尽量使用字符串方法）",
+  // 9.考察split方法
+  "let str = 'ab&def&cc';，创建一个新数组arrNew，值为[ab, def, cc]？（尽量使用字符串方法）",
+  // 10.考察toLowerCase和toUpperCase方法
+  " let str = 'ABC',let str2 = 'abc';，分别将str转成小写，将str2转成大写？（尽量使用字符串方法）",
+   // 11.考察trim方法
+   " var str = '       Hello World!        ';，删除str两端的空白符？（尽量使用字符串方法）",
 ];
